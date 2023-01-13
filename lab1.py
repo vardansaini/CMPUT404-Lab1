64677f01fb14abfe4af90372d58d87bff349f370
vardansaini <vardan1@ualberta.ca>
2023-01-13 13:43:00 -0700

    after update

--- lab1.py
+++ lab1.py
@@ -5,3 +5,13 @@ print(requests.__version__)
 print(requests.get('https://www.google.com'))
 
 print(requests.get('https://www.google.com').content)
+
+print('######################## VARDAN LAB 1 ############################')
+
+print(requests.get(
+    'https://raw.githubusercontent.com/vardansaini/CMPUT404-Lab1/main/lab1.py').content)
+
+print('######################## VARDAN LAB 1 text ############################')
+
+print(requests.get(
+    'https://raw.githubusercontent.com/vardansaini/CMPUT404-Lab1/main/lab1.py').text)
