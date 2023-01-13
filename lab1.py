32081595ea91c5a77c3a1cff487e8113961fbcf5
vardansaini <vardan1@ualberta.ca>
2023-01-13 15:52:33 -0700

    minor fix

--- lab1.py
+++ lab1.py
@@ -2,11 +2,15 @@ import requests
 
 print(requests.__version__)
 
+print('######################## VARDAN LAB 1 Google ############################')
+
 print(requests.get('https://www.google.com'))
 
 print(requests.get('https://www.google.com').content)
 
-print('######################## VARDAN LAB 1 ############################')
+print(requests.get('https://www.google.com').text)
+
+print('######################## VARDAN LAB 1 Github ############################')
 
 print(requests.get(
     'https://raw.githubusercontent.com/vardansaini/CMPUT404-Lab1/main/lab1.py').content)
